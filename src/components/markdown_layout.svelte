af0b30e9ffcfc7b55b19cb5a023b28b3acc20aac
Tobias Bora <tobias.bora@gmail.com>
2024-12-12 22:11:30 +0100

    Show bug with rehype-katex

--- src/components/markdown_layout.svelte
+++ src/components/markdown_layout.svelte
@@ -4,6 +4,7 @@
 
 <svelte:head>
   <title>{title}</title>
+  <link href="https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css" rel="stylesheet">
 </svelte:head>
 
 <div class="prose max-w-none">
--- svelte.config.js
+++ svelte.config.js
@@ -2,20 +2,35 @@ import { mdsvex } from "mdsvex";
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+import rehypeKatex from "rehype-katex";
+import remarkMath from 'remark-math'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://svelte.dev/docs/kit/integrations
-    // for more information about preprocessors
-    preprocess: [vitePreprocess(), mdsvex()],
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: [
+    vitePreprocess(),
+    mdsvex({
+      remarkPlugins: [
+        remarkMath,
+      ],
+      rehypePlugins: [
+        rehypeKatex,
+      ],
+      extensions: ['.svx', '.md'],
+      layout: "./src/components/markdown_layout.svelte",
+    })
+  ],
 
-    kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	},
+  kit: {
+    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+    adapter: adapter()
+  },
 
-    extensions: [".svelte", ".svx"]
+  extensions: [".svelte", ".svx"]
 };
 
 export default config;
